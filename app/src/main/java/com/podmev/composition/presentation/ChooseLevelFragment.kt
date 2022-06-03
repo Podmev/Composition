90b1e7cda73532f18f223c5c91977a0a49983be4
Dmitriy Pozdin <Dmitriy.Pozdin@bia-tech.ru>
2022-06-04 02:00:38 +0300

    goto game fragment from choose level fragment

--- app/src/main/java/com/podmev/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/podmev/composition/presentation/ChooseLevelFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.podmev.composition.R
 import com.podmev.composition.databinding.FragmentChooseLevelBinding
+import com.podmev.composition.domain.entity.Level
 
 class ChooseLevelFragment : Fragment() {
     private var _binding: FragmentChooseLevelBinding? = null
@@ -24,6 +25,23 @@ class ChooseLevelFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding){
+            buttonLevelTest.setOnClickListener { launchGameFragment(Level.TEST)}
+            buttonLevelEasy.setOnClickListener { launchGameFragment(Level.EASY)}
+            buttonLevelNormal.setOnClickListener { launchGameFragment(Level.NORMAL)}
+            buttonLevelHard.setOnClickListener { launchGameFragment(Level.HARD)}
+        }
+    }
+
+    private fun launchGameFragment(level: Level){
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFragment.newInstance(level))
+            .addToBackStack(null)
+            .commit()
     }
 
     companion object{
--- app/src/main/java/com/podmev/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/podmev/composition/presentation/GameFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.podmev.composition.R
 import com.podmev.composition.databinding.FragmentGameBinding
+import com.podmev.composition.domain.entity.Level
+
 
 class GameFragment : Fragment() {
+    private lateinit var level: Level
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +31,21 @@ class GameFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun parseArgs(){
+       level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+
+    companion object{
+        private const val KEY_LEVEL = "level"
+
+        fun newInstance(level: Level): GameFragment{
+            return GameFragment().apply{
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 }
