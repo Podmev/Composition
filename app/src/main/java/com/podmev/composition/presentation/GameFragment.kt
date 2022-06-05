d413732e8a10abbfa8250fe6c4a34b3ffc27b07c
Dmitriy Pozdin <Dmitriy.Pozdin@bia-tech.ru>
2022-06-06 00:09:59 +0300

    finished GameFragment

--- app/src/main/java/com/podmev/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/podmev/composition/presentation/GameFragment.kt
@@ -1,16 +1,18 @@
 package com.podmev.composition.presentation
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
 import com.podmev.composition.R
 import com.podmev.composition.databinding.FragmentGameBinding
 import com.podmev.composition.domain.entity.GameResult
-import com.podmev.composition.domain.entity.GameSettings
 import com.podmev.composition.domain.entity.Level
 
 
@@ -21,6 +23,17 @@ class GameFragment : Fragment() {
             this,
             AndroidViewModelFactory.getInstance(requireActivity().application)
         )[GameViewModel::class.java]
+    }
+
+    private val tvOptions: MutableList<TextView> by lazy {
+        mutableListOf<TextView>().apply {
+            add(binding.tvOption1)
+            add(binding.tvOption2)
+            add(binding.tvOption3)
+            add(binding.tvOption4)
+            add(binding.tvOption5)
+            add(binding.tvOption6)
+        }
     }
 
     private var _binding: FragmentGameBinding? = null
@@ -47,32 +60,74 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(binding){
-            tvOption1.setOnClickListener{
-                launchGameFinishedFragment(GameResult(
-                    winner = true,
-                    countOfRightAnswers = 5,
-                    countOfQuestions = 6,
-                    gameSettings = GameSettings(0, 0, 0, 0)
-                ))
+        observeViewModel()
+        setClickListenersToOptions()
+        viewModel.startGame(level)
+    }
+
+    private fun setClickListenersToOptions(){
+        for(tvOption in tvOptions){
+            tvOption.setOnClickListener{
+                viewModel.chooseAnswer(tvOption.text.toString().toInt())
             }
         }
     }
 
-    private fun parseArgs(){
-       requireArguments().getParcelable<Level>(KEY_LEVEL)?.let {
-           level = it
-       }
+    private fun observeViewModel() {
+        viewModel.question.observe(viewLifecycleOwner) {
+            binding.tvSum.text = it.sum.toString()
+            binding.tvLeftNumber.text = it.visibleNumber.toString()
+            for (i in 0 until tvOptions.size) {
+                tvOptions[i].text = it.options[i].toString()
+            }
+        }
+        viewModel.percentOrRightAnswers.observe(viewLifecycleOwner) {
+            binding.progressBar.setProgress(it, true)
+        }
+        viewModel.enoughCount.observe(viewLifecycleOwner) {
+            binding.tvAnswersProgress.setTextColor(getColorByState(it))
+        }
+        viewModel.enoughPercent.observe(viewLifecycleOwner) {
+            val color = getColorByState(it)
+            binding.progressBar.progressTintList = ColorStateList.valueOf(color)
+        }
+        viewModel.formattedTime.observe(viewLifecycleOwner){
+            binding.tvTimer.text = it
+        }
+        viewModel.minPercent.observe(viewLifecycleOwner){
+            binding.progressBar.secondaryProgress = it
+        }
+        viewModel.gameResult.observe(viewLifecycleOwner){
+            launchGameFinishedFragment(it)
+        }
+        viewModel.progressAnswers.observe(viewLifecycleOwner){
+            binding.tvAnswersProgress.text = it
+        }
     }
 
-    private fun launchGameFinishedFragment(gameResult: GameResult){
+    private fun getColorByState(goodState: Boolean): Int {
+        val colorResId = if (goodState) {
+            android.R.color.holo_green_light
+        } else {
+            android.R.color.holo_red_light
+        }
+        return ContextCompat.getColor(requireContext(), colorResId)
+    }
+
+    private fun parseArgs() {
+        requireArguments().getParcelable<Level>(KEY_LEVEL)?.let {
+            level = it
+        }
+    }
+
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
         requireActivity().supportFragmentManager.beginTransaction()
             .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
             .addToBackStack(null)
             .commit()
     }
 
-    companion object{
+    companion object {
         private const val KEY_LEVEL = "level"
         const val NAME = "GameFragment"
 
