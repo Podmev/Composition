d83589e57a5a0132b10de528931eb67a797af605
Dmitriy Pozdin <Dmitriy.Pozdin@bia-tech.ru>
2022-06-06 00:57:55 +0300

    use GameViewModelFactory

--- app/src/main/java/com/podmev/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/podmev/composition/presentation/GameFragment.kt
@@ -18,11 +18,12 @@ import com.podmev.composition.domain.entity.Level
 
 class GameFragment : Fragment() {
     private lateinit var level: Level
+    private val gameViewModelFactory by lazy {
+        GameViewModelFactory(requireActivity().application, level)
+    }
+
     private val viewModel: GameViewModel by lazy {
-        ViewModelProvider(
-            this,
-            AndroidViewModelFactory.getInstance(requireActivity().application)
-        )[GameViewModel::class.java]
+        ViewModelProvider(this, gameViewModelFactory)[GameViewModel::class.java]
     }
 
     private val tvOptions: MutableList<TextView> by lazy {
@@ -62,7 +63,6 @@ class GameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         observeViewModel()
         setClickListenersToOptions()
-        viewModel.startGame(level)
     }
 
     private fun setClickListenersToOptions() {
--- app/src/main/java/com/podmev/composition/presentation/GameViewModel.kt
+++ app/src/main/java/com/podmev/composition/presentation/GameViewModel.kt
@@ -1,10 +1,12 @@
 package com.podmev.composition.presentation
 
 import android.app.Application
+import android.content.Context
 import android.os.CountDownTimer
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.podmev.composition.R
 import com.podmev.composition.data.GameRepositoryImpl
 import com.podmev.composition.domain.entity.GameResult
@@ -14,11 +16,11 @@ import com.podmev.composition.domain.entity.Question
 import com.podmev.composition.domain.usecase.GenerateQuestionUseCase
 import com.podmev.composition.domain.usecase.GetGameSettingsUseCase
 
-class GameViewModel(application: Application) : AndroidViewModel(application) {
+class GameViewModel(
+    private val application: Application,
+    private val level: Level
+    ) : ViewModel() {
     private lateinit var gameSettings: GameSettings
-    private lateinit var level: Level
-
-    private val context = application
 
     private val repository = GameRepositoryImpl
     private val generateQuestionUseCase = GenerateQuestionUseCase(repository)
@@ -62,8 +64,12 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
     private var countOfRightAnswers = 0
     private var countOfQuestions = 0
 
-    fun startGame(level: Level) {
-        getGameSettings(level)
+    init{
+        startGame()
+    }
+
+    private fun startGame() {
+        getGameSettings()
         startTimer()
         generateQuestion()
         updateProgress()
@@ -79,7 +85,7 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
         val percent = calculatePercentOfRightAnswers()
         _percentOrRightAnswers.value = percent
         _progressAnswers.value = String.format(
-            context.resources.getString(R.string.progress_answers),
+            application.resources.getString(R.string.progress_answers),
             countOfRightAnswers,
             gameSettings.minCountOfRightAnswers
         )
@@ -102,8 +108,7 @@ class GameViewModel(application: Application) : AndroidViewModel(application) {
         countOfQuestions++
     }
 
-    private fun getGameSettings(level: Level) {
-        this.level = level
+    private fun getGameSettings() {
         this.gameSettings = getGameSettingsUseCase(level)
         _minPercent.value = gameSettings.minPercentOfRightAnswers
     }
